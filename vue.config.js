3ab2a777f437586377766f694b44ecfcd3da5242
NgallaJuke <ngalladiouck@gmail.com>
2020-03-19 01:17:15 +0000

    last fixes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+const path = require("path");
 module.exports = {
-  lintOnSave: false
+  outputDir: path.resolve(__dirname, "../server/public"),
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000"
+      }
+    }
+  }
 };
